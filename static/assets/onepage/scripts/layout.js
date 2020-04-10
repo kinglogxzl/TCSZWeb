bd7c56dd92016fa6b83e832f8d82cdf0bd08a163
Kinglog <391275002@qq.com>
2020-04-10 14:34:29 +0800

    logo补充

--- static/assets/onepage/scripts/layout.js
+++ static/assets/onepage/scripts/layout.js
@@ -138,11 +138,6 @@ var Layout = function () {
         });
     }
 
-    var handleTwitter = function () {
-        !function(d,s,id){
-            var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}
-        }(document,"script","twitter-wjs");
-    }  
         
     function valignCenterMessageFunction () {
          MessageCurrentElemHeight = $(".message-block .valign-center-elem").height();
