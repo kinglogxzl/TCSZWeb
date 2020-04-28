4579db65f0ef9a8acaf38bc051adb66e3498c689
Jxx <1039227140@qq.com>
2020-04-28 10:23:30 +0800

    景区活动完成

--- TCSZWeb/urls.py
+++ TCSZWeb/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
     path('vacation/play/detail/<str:name>', tcsz.play_detail),
     path('activity/new/detail/<str:name>', tcsz.new_detail),
     path('activity/notice/detail/<str:name>', tcsz.notice_detail),
+    path('activity/activity/detail/<str:name>', tcsz.activity_detail),
     path('trip/traffic',tcsz.traffic),
     path('trip/strategy',tcsz.strategy),
     path('video/video',tcsz.video)
